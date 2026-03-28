901ee8017aa69e8ddfb831da3b1330610f222074
Siddhantv1 <siddhaaaaaaant@gmail.com>
2026-03-28 23:45:14 +0530

    update: add voice router in api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import config from './config.js';
 import honeypotRouter from './routes/honeypot.js';
+import voiceRouter from './routes/voice.js';
 
 const app = express();
 
@@ -16,7 +17,7 @@ app.use((req, res, next) => {
 });
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 
 //middleware for debug logger
 app.use((req, res, next) => {
@@ -39,6 +40,7 @@ app.use((err, req, res, next) => {
 
 // Routes
 app.use('/api', honeypotRouter);
+app.use('/api', voiceRouter);
 
 // Health check
 app.get('/health', (req, res) => {
